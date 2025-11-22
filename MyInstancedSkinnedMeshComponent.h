6f85c9bda3664913849b43c803978c1eb930c008
Fahim Faisal <fahimfaisalrafi@gmail.com>
2025-11-22 16:21:49 +0600

    Initial commit

---
+++ MyInstancedSkinnedMeshComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/InstancedSkinnedMeshComponent.h"
+#include "MyInstancedSkinnedMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAT_API UMyInstancedSkinnedMeshComponent : public UInstancedSkinnedMeshComponent
+{
+	GENERATED_BODY()
+	
+};
